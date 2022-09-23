4cff2d20537d61a1d1c329d2ad2de171adc5e85d
josephjacksonw <josephjacksonw@gmail.com>
2022-09-23 17:05:49 -0400

    bread portion of ui can now catch bugs

--- Bakery/Bakery.cs
+++ Bakery/Bakery.cs
@@ -15,9 +15,33 @@ namespace Store
       Food pastry = new Food("pastry", 2, 3, 5);
       List<Food> Foods = new List<Food>() {bread, pastry};
       Console.WriteLine("Welcome to Pierre's Bakery, bread is $5 a loaf but every 2 you buy, you can get a 3rd for free. Pastries are $2 but you can also buy 3 pastries for $5");
-
+      
       Console.WriteLine("How many loaves of bread would you like to order?");
-      int breadNum = Convert.ToInt32(Console.ReadLine());
+      int breadNum = 0;
+      bool buul = true;
+      while (buul)
+      {
+        buul = false;
+        try
+        {
+          breadNum = Convert.ToInt32(Console.ReadLine());
+        }
+        catch
+        {
+          Console.WriteLine("Sorry that's not an integer number. Enter 0 if you wouldn't like to order any bread");
+          buul = true;
+        }
+        if (breadNum < 0)
+        {
+          Console.WriteLine("Sorry we can't buy your bread, how many loaves of our bread would you like to buy?");
+          buul = true;
+        }
+      }
+    
+      
+
+      
+      
       Console.WriteLine("How many pastries would you like to order?");
       int pastryNum = Convert.ToInt32(Console.ReadLine());
       int orderTotal = bread.cost(breadNum) + pastry.cost(pastryNum);
