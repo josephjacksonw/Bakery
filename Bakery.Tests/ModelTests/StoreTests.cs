bda3cdeff4f19e0f33adaf1839688b2faf380388
josephjacksonw <josephjacksonw@gmail.com>
2022-10-02 23:54:55 -0400

    all 6 tests good pass

--- Bakery.Tests/ModelTests/StoreTests.cs
+++ Bakery.Tests/ModelTests/StoreTests.cs
@@ -11,40 +11,40 @@ namespace Store.Models.Tests
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
       Bread newBread = new Bread("newBread", 1, 1, 1);
-      Assert.AreEqual(typeof(string), newBread.GetType());
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
       Pastry newPastry = new Pastry("newPastry", 1, 1, 1);
-      Assert.AreEqual(typeof(string), newPastry.GetType());
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
     [TestMethod]
     public void cost_returnsBreadCostWithoutDiscounts_Int()
     {
       Bread newBread = new Bread("newBread", 10, 10, 80);
-      Assert.AreEqual(60, newBread.cost(7));
+      Assert.AreEqual(60, newBread.cost(6));
     }
 
     [TestMethod]
     public void cost_returnsPastryCostWithoutDiscounts_Int()
     {
       Pastry newPastry = new Pastry("newPastry", 1, 8, 5);
-      Assert.AreEqual(6, newPastry.cost(7));
+      Assert.AreEqual(6, newPastry.cost(6));
     }
 
     [TestMethod]
     public void cost_returnsBreadCostWithDiscounts_Int()
     {
       Bread newBread = new Bread("newBread", 10, 10, 80);
-      Assert.AreEqual(170, newBread.cost(22));
+      Assert.AreEqual(170, newBread.cost(21));
     }
 
     [TestMethod]
     public void cost_returnsPastryCostWithDiscounts_Int()
     {
       Pastry newPastry = new Pastry("newPastry", 1, 8, 5);
-      Assert.AreEqual(11, newPastry.cost(18));
+      Assert.AreEqual(11, newPastry.cost(17));
     }
   }
 }
